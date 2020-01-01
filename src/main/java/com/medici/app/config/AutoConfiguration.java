bce9996ab326b7d6748a7f9b2d3ee56463b091ae
jamesmedice <U0I2747 tiago.medice@gft.com>
2020-01-01 15:07:54 +0100

    release

--- src/main/java/com/medici/app/config/AutoConfiguration.java
+++ src/main/java/com/medici/app/config/AutoConfiguration.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.medici.app.entity.Company;
+import com.medici.app.entity.Invoice;
 import com.medici.app.repository.CompanyRepository;
+import com.medici.app.repository.InvoiceRepository;
 
 @Configuration
-@EntityScan(basePackageClasses = { Company.class })
-@EnableJpaRepositories(basePackageClasses = { CompanyRepository.class })
+@EntityScan(basePackageClasses = { Company.class, Invoice.class })
+@EnableJpaRepositories(basePackageClasses = { CompanyRepository.class, InvoiceRepository.class })
 @EnableAutoConfiguration
 public class AutoConfiguration {
 
